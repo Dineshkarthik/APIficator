ad9f357c96033364abb544830b89830b794b0d58
Dineshkarthik <dinesh.kartoon@gmail.com>
2016-10-14 20:57:09 +0530

    Adding Doc Strings

--- apificator.py
+++ apificator.py
@@ -10,7 +10,7 @@ from sqlalchemy import *
 
 
 class ApiGenerator:
-"""Class used to generate code for API on the fly"""
+    """Class used to generate code for API on the fly"""
     def begin(self, tab="\t"):
         self.code = []
         self.tab = tab
