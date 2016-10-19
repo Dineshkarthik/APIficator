c1231b6aaed14c5d3192139764a4f5f4f0baa25a
Dineshkarthik <dinesh.kartoon@gmail.com>
2016-10-19 15:59:38 +0530

    added filtering with params

--- apificator.py
+++ apificator.py
@@ -1,7 +1,6 @@
 """Tornado App to Generate Rest API for existing Database."""
 import sys
 import string
-import yaml
 import tornado.escape
 import tornado.ioloop
 import tornado.web
@@ -15,7 +14,7 @@ from sqlalchemy.orm import *
 
 
 class ApiGenerator:
-    """Class used to generate code for API on the fly"""
+    """Class used to generate code for API on the fly."""
 
     def begin(self, tab="\t"):
         self.code = []
@@ -46,11 +45,10 @@ class ApiGenerator:
 
 
 def new_alchemy_encoder():
-    """class to convert sqlalchemy results to json."""
+    """Function to convert sqlalchemy results to json."""
     _visited_objs = []
 
     class AlchemyEncoder(json.JSONEncoder):
-
         def default(self, obj):
             if isinstance(obj.__class__, DeclarativeMeta):
                 # don't re-visit self
@@ -60,13 +58,16 @@ def new_alchemy_encoder():
 
                 # an SQLAlchemy class
                 fields = {}
-                for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                for field in [x for x in dir(obj)
+                              if not x.startswith('_') and x != 'metadata']:
                     fields[field] = obj.__getattribute__(field)
                 # a json-encodable dict
                 return fields
 
             return json.JSONEncoder.default(self, obj)
+
     return AlchemyEncoder
+
 
 db = create_engine(sys.argv[1], echo=False)
 Base = declarative_base()
@@ -79,39 +80,55 @@ ar = []
 c = ApiGenerator()
 c.begin(tab="    ")
 for t in tables:
-    c.write("class " + t + "(Base):\n")
-    c.indent()
-    c.write("__table__ = Base.metadata.tables['" + t + "']")
-    c.dedent()
-    c.newline(no=2)
+    c1 = ApiGenerator()
+    c1.begin(tab="    ")
+    c1.write("class " + t + "(Base):\n")
+    c1.indent()
+    c1.write("__table__ = Base.metadata.tables['" + t + "']")
+    c1.dedent()
+    c1.newline(no=2)
+    exec (c1.end())
+    key = inspect(eval(t)).primary_key[0].key
     c.write("class " + t + "Details" + "(tornado.web.RequestHandler):\n")
     c.indent()
     c.write("def get(self):\n")
     c.indent()
-    c.write("p_id = (self.request.uri).rsplit('/', 1)[1]\n")
-    c.write("if p_id:\n")
+    c.write("args = self.request.arguments\n")
+    c.write("if args:\n")
     c.indent()
-    c.write("""result = session.query(""" + t + """).filter_by(id=p_id).first()
-            self.set_header(
-                "Content-Type", 'application/json; charset="utf-8"')
-            self.write(json.dumps(
-                result, cls=new_alchemy_encoder(), check_circular=False))\n""")
+    c.write("a = []\n")
+    c.write("t = '" + t + "'\n")
+    c.write("for k, v in args.iteritems():\n")
+    c.indent()
+    c.write("""a.append(" " + t + "." + k + "='" + v[0] + "'")\n""")
+    c.dedent()
+    c.write("""result = session.query(""" + t + """).filter(
+                    (','.join(a).replace(",", "AND"))).all()\n""")
     c.dedent()
     c.write("else:\n")
     c.indent()
-    c.write("""result = session.query(""" + t + """).all()
-            self.set_header(
-                "Content-Type", 'application/json; charset="utf-8"')
-            self.write(json.dumps(
+    c.write("p_id = (self.request.uri).rsplit('/', 1)[1]\n")
+    c.write("if p_id:\n")
+    c.indent()
+    c.write("""result = session.query(""" + t + """).filter_by(""" + key +
+            """=p_id).first()\n""")
+    c.dedent()
+    c.write("else:\n")
+    c.indent()
+    c.write("""result = session.query(""" + t + """).all()\n""")
+    c.dedent()
+    c.dedent()
+    c.write("""self.set_header(
+                "Content-Type", 'application/json; charset="utf-8"')\n""")
+    c.write("""self.write(json.dumps(
                 result, cls=new_alchemy_encoder(), check_circular=False))""")
     c.dedent()
     c.dedent()
-    c.dedent()
     c.newline(no=2)
-    ar.append('(r"/' + t + "/" '", ' + t + 'Details)')
-    ar.append('(r"/' + t + "/[0-9]*" '", ' + t + 'Details)')
+    ar.append('(r"/' + t + "/?" '", ' + t + 'Details)')
+    ar.append('(r"/' + t + "/[a-zA-Z0-9]+" '", ' + t + 'Details)')
 c.write("application = tornado.web.Application([" + ','.join(ar) + "])")
-exec(c.end())
+exec (c.end())
 
 if __name__ == "__main__":
     application.listen(8800)
